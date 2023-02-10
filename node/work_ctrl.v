1b85e0a68648cf517eb455e3b500150fc3c40a11
liyt <491849663@qq.com>
2023-02-10 09:57:39 +0800

    x_s y_s cal

--- node/work_ctrl.v
+++ node/work_ctrl.v
@@ -201,10 +201,10 @@ always @(posedge clk or negedge rst_n) begin
              (((cs == CODE_P) || (cs == P_WAIT)) && (ns == CODE_P)) ||
              ((cs == CLEAR) && (ns == CLEAR))) begin
         neu_id <= neu_id + 1'b1;
-        if (x_s < x_out[SW/3-1:0]) begin // [x_out-1 : 0]
+        if (x_s < x_out[SW/3-1:0] - 1) begin // [x_out-1 : 0]
             x_s <= x_s + 1'b1;
         end
-        else if (y_s < y_out[SW/3-1:0]) begin // [y_out-1 : 0]
+        else if (y_s < y_out[SW/3-1:0] - 1) begin // [y_out-1 : 0]
             x_s <= {(SW/3){1'b0}};
             y_s <= y_s + 1'b1;
         end
