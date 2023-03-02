35d90b2fa62cc2fb6f6385ddb9342645cef42331
liyt <491849663@qq.com>
2023-03-02 09:13:38 +0800

    tik 13

--- tb/host/run_pcss_fpga.py
+++ tb/host/run_pcss_fpga.py
@@ -1,8 +1,12 @@
+import os
+import socket
+import time
+import struct
+import sys
+
 import numpy as np
 import math
 import pickle
-
-global debug
 
 class Node():
     def __init__(self):
@@ -303,6 +307,38 @@ class Configuration(object):
         f.close()
         print('config done')
 
+    def gen_clear(self,filename="spike.txt"):
+        ## gen clear file
+        f=open(filename,'w')
+        for node in self.node_list:
+            flit_head = (node.connect << 59) + (node.pclass << 56) \
+                + (node.dst_y << 52) + (node.dst_x << 48) + (node.dst_r2 << 42) + (node.dst_r1 << 36)
+            '''
+            nm_status:
+                [0] enable bit
+                [1] clear bit
+                [2:3] spike code : 00 LIF, 01 Count, 10 Poisson
+                [4] reset bit : 0 zero, 1 -vth
+            addr : 0x0
+            '''
+            # clear
+            node.enable = 0
+            node.clear = 1
+            waddr = 0
+            tmp = (node.reset << 4) + (node.spike_code << 2) + (node.clear << 1) + node.enable
+            ss = "%016x" % (flit_head + (waddr << 21) + tmp)
+            f.write(ss+'\n')
+            # enable
+            node.enable = 1
+            node.clear = 0
+            waddr = 0
+            tmp = (node.reset << 4) + (node.spike_code << 2) + (node.clear << 1) + node.enable
+            ss = "%016x" % (flit_head + (waddr << 21) + tmp)
+            f.write(ss+'\n')
+
+        f.close()
+        print('clear done')
+
 class Input_Node():
     def __init__(self):
         # packet par
@@ -348,12 +384,8 @@ class Input_Node():
         self.reset = reset
 
 
-    def gen_input(self,filename="config.txt",feature_map=[]):
+    def gen_input(self,filename="spike.txt",feature_map=[]):
         f=open(filename,'a')
-        # debug
-        if debug:
-            ss = "====input===="
-            f.write(ss+'\n')
 
         # unenable
         self.pclass = 0b110 # write
@@ -403,7 +435,7 @@ class Tik():
         self.tik_len = tik_len
         self.pclass = 0b011 # tik
 
-    def gen_input(self,filename="config.txt"):
+    def gen_input(self,filename="spike.txt"):
         f=open(filename,'a')
 
         # gen tik
@@ -419,142 +451,275 @@ class Tik():
         f.close()
         print('tik done')
 
-if __name__ == "__main__":
-    debug = False    
-    #---------------------
-    #       node
-    #---------------------
-    tik_num = 13
-    tik_len = 524288 # 32 bit
-    # spk node par
-    reset = 1
-    vth = 0 # TODO
-    leak = 255 # TODO
-    # node
-    node_list = []
+class PCSS():
+    def __init__(self):
+        # spk node par
+        self.reset = 1
+        self.vth = 0 # TODO
+        self.leak = 255 # TODO
+        # tik par
+        self.tik_num = 8
+        self.tik_len = 1048576 # 32 bit
+        # node
+        self.node_list = []
+        self.conf = None
+        self.tik_init = Tik(tik_num=0,tik_len=self.tik_len)
+        self.tik_send = Tik(tik_num=self.tik_num,tik_len=self.tik_len)
 
-    # read weight
-    with open('track_weight.pkl', 'rb') as f:
-        track_wgt = pickle.load(f)
+    def init_pcss(self):
 
-    # read vth
-    with open('track_vth.pkl', 'rb') as f:
-        track_vth = pickle.load(f)
+        # read weight
+        with open('track_weight.pkl', 'rb') as f:
+            track_wgt = pickle.load(f)
 
-    # spike coding node
-    for x in range(4):
-        for y in range(4):
-            if x == 3:
-                if y == 3:
-                    neu_num = 63*63
-                    xout = 63
-                    yout = 63
+        # read vth
+        with open('track_vth.pkl', 'rb') as f:
+            track_vth = pickle.load(f)
+
+        # spike coding node
+        for x in range(4):
+            for y in range(4):
+                if x == 3:
+                    if y == 3:
+                        neu_num = 63*63
+                        xout = 63
+                        yout = 63
+                    else :
+                        neu_num = 63*64
+                        xout = 63
+                        yout = 64
                 else :
-                    neu_num = 63*64
-                    xout = 63
-                    yout = 64
-            else :
-                if y == 3:
-                    neu_num = 64*63
-                    xout = 64
-                    yout = 63
-                else :
-                    neu_num = 64*64
-                    xout = 64
-                    yout = 64
-            
-            node_number = y*4 + x
+                    if y == 3:
+                        neu_num = 64*63
+                        xout = 64
+                        yout = 63
+                    else :
+                        neu_num = 64*64
+                        xout = 64
+                        yout = 64
+                
+                node_number = y*4 + x
+                node_tmp = Node()
+                node_tmp.set_pclass(package_class='write')
+                node_tmp.set_node_loc(node_x=1,node_y=0,node_number=node_number)
+                node_tmp.set_neu_par(spike_code='Count',reset=self.reset,vth=self.vth,leak=self.leak)
+                node_tmp.set_neu_num(neu_num=neu_num)
+                node_tmp.set_conv(xin=0,yin=0,xstart=64*x,ystart=64*y,xout=xout,yout=yout,zout=0,xk=0,yk=0,pad=0,stride=4) #TODO
+                node_tmp.set_dst_mem(dst_node_x=0,dst_node_y=0,dst_node_number=8)
+                self.node_list.append(node_tmp)
+
+        # node par
+        vth = track_vth[0]
+        leak = 0
+
+        # conv1 node
+        node_num = 8
+        neu_num = 64*64
+        for n in range(0, node_num):
             node_tmp = Node()
             node_tmp.set_pclass(package_class='write')
-            node_tmp.set_node_loc(node_x=1,node_y=0,node_number=node_number)
-            node_tmp.set_neu_par(spike_code='Count',reset=reset,vth=vth,leak=leak)
+            node_tmp.set_node_loc(node_x=0,node_y=0,node_number=n)
+            node_tmp.set_neu_par(spike_code='LIF',reset=self.reset,vth=vth[n],leak=leak)
             node_tmp.set_neu_num(neu_num=neu_num)
-            node_tmp.set_conv(xin=0,yin=0,xstart=64*x,ystart=64*y,xout=xout,yout=yout,zout=0,xk=0,yk=0,pad=0,stride=4) #TODO
-            node_tmp.set_dst_mem(dst_node_x=0,dst_node_y=0,dst_node_number=8)
-            node_list.append(node_tmp)
+            node_tmp.set_conv(xin=255,yin=255,xstart=0,ystart=0,xout=64,yout=64,zout=n,xk=3,yk=3,pad=0,stride=4) #TODO
+            node_tmp.set_wgt_mem(weight_list=track_wgt[0][n])
+            node_tmp.set_dst_mem(dst_node_x=0,dst_node_y=1,dst_node_number=16)
+            self.node_list.append(node_tmp)
 
-    # node par
-    vth = track_vth[0]
-    leak = 0
+        # node par
+        vth = track_vth[1]
+        leak = 0
 
-    # conv1 node
-    node_num = 8
-    neu_num = 64*64
-    for n in range(0, node_num):
-        node_tmp = Node()
-        node_tmp.set_pclass(package_class='write')
-        node_tmp.set_node_loc(node_x=0,node_y=0,node_number=n)
-        node_tmp.set_neu_par(spike_code='LIF',reset=reset,vth=vth[n],leak=leak)
-        node_tmp.set_neu_num(neu_num=neu_num)
-        node_tmp.set_conv(xin=255,yin=255,xstart=0,ystart=0,xout=64,yout=64,zout=n,xk=3,yk=3,pad=0,stride=4) #TODO
-        node_tmp.set_wgt_mem(weight_list=track_wgt[0][n])
-        node_tmp.set_dst_mem(dst_node_x=0,dst_node_y=1,dst_node_number=16)
-        node_list.append(node_tmp)
+        # conv2 node
+        node_num = 16
+        neu_num = 31*31
+        for n in range(0, node_num):
+            node_tmp = Node()
+            node_tmp.set_pclass(package_class='write')
+            node_tmp.set_node_loc(node_x=0,node_y=1,node_number=n)
+            node_tmp.set_neu_par(spike_code='LIF',reset=self.reset,vth=vth[n],leak=leak)
+            node_tmp.set_neu_num(neu_num=neu_num)
+            node_tmp.set_conv(xin=64,yin=64,xstart=0,ystart=0,xout=31,yout=31,zout=n,xk=3,yk=3,pad=0,stride=2)
+            node_tmp.set_wgt_mem(weight_list=track_wgt[1][n])
+            node_tmp.set_dst_mem(dst_node_x=1,dst_node_y=1,dst_node_number=8)
+            self.node_list.append(node_tmp)
 
-    # node par
-    vth = track_vth[1]
-    leak = 0
+        # node par
+        vth = track_vth[2]
+        leak = 0
 
-    # conv2 node
-    node_num = 16
-    neu_num = 31*31
-    for n in range(0, node_num):
-        node_tmp = Node()
-        node_tmp.set_pclass(package_class='write')
-        node_tmp.set_node_loc(node_x=0,node_y=1,node_number=n)
-        node_tmp.set_neu_par(spike_code='LIF',reset=reset,vth=vth[n],leak=leak)
-        node_tmp.set_neu_num(neu_num=neu_num)
-        node_tmp.set_conv(xin=64,yin=64,xstart=0,ystart=0,xout=31,yout=31,zout=n,xk=3,yk=3,pad=0,stride=2)
-        node_tmp.set_wgt_mem(weight_list=track_wgt[1][n])
-        node_tmp.set_dst_mem(dst_node_x=1,dst_node_y=1,dst_node_number=8)
-        node_list.append(node_tmp)
+        # conv3 node
+        node_num = 8
+        neu_num = 29*29
+        for n in range(0, node_num):
+            node_tmp = Node()
+            node_tmp.set_pclass(package_class='write')
+            node_tmp.set_node_loc(node_x=1,node_y=1,node_number=n)
+            node_tmp.set_neu_par(spike_code='LIF',reset=self.reset,vth=vth[n],leak=leak)
+            node_tmp.set_neu_num(neu_num=neu_num)
+            node_tmp.set_conv(xin=31,yin=31,xstart=0,ystart=0,xout=29,yout=29,zout=n,xk=3,yk=3,pad=0,stride=1)
+            node_tmp.set_wgt_mem(weight_list=track_wgt[2][n])
+            node_tmp.set_dst_mem(dst_node_x=2,dst_node_y=1,dst_node_number=1)
+            self.node_list.append(node_tmp)
 
-    # node par
-    vth = track_vth[2]
-    leak = 0
-
-    # conv3 node
-    node_num = 8
-    neu_num = 29*29
-    for n in range(0, node_num):
-        node_tmp = Node()
-        node_tmp.set_pclass(package_class='write')
-        node_tmp.set_node_loc(node_x=1,node_y=1,node_number=n)
-        node_tmp.set_neu_par(spike_code='LIF',reset=reset,vth=vth[n],leak=leak)
-        node_tmp.set_neu_num(neu_num=neu_num)
-        node_tmp.set_conv(xin=31,yin=31,xstart=0,ystart=0,xout=29,yout=29,zout=n,xk=3,yk=3,pad=0,stride=1)
-        node_tmp.set_wgt_mem(weight_list=track_wgt[2][n])
-        node_tmp.set_dst_mem(dst_node_x=2,dst_node_y=1,dst_node_number=1)
-        node_list.append(node_tmp)
-
-    # gen config
-    conf = Configuration(node_list=node_list)
-    conf.gen_conf()
+        # gen config
+        self.conf = Configuration(node_list=self.node_list)
+        self.conf.gen_conf()
+        self.tik_init.gen_input(filename="config.txt")
 
     #---------------------
     #       gen spk
     #---------------------
-    with open('track_fm_255.pkl', 'rb') as f:
-        track_fm = pickle.load(f)
+    def gen_spk(self,track_fm=[]):
 
-    # track_fm = [[3 for i in range(127)] for j in range(127)]
+        # clear
+        self.conf.gen_clear(filename="spike.txt")
 
-    for x in range(4):
-        for y in range(4):
-            if x == 3:
-                dx = 63
-            else :
-                dx = 64
-            if y == 3:
-                dy = 63
-            else :
-                dy = 64
-            node_number = y*4 + x
-            node_tmp = Input_Node()
-            node_tmp.set_node_loc(node_x=1,node_y=0,node_number=node_number)
-            node_tmp.set_neu_par(spike_code='Count',reset=reset)
-            fm = [track_fm[i][64*x : 64*x+dx] for i in range(64*y, 64*y+dy)]
-            node_tmp.gen_input(filename="config.txt",feature_map=fm)
-            
-    tik_send = Tik(tik_num=tik_num,tik_len=tik_len)
-    tik_send.gen_input(filename="config.txt")
+        # load fm
+        if track_fm is not None:
+            with open('track_fm_255.pkl', 'rb') as f:
+                track_fm = pickle.load(f)
+
+        for x in range(4):
+            for y in range(4):
+                if x == 3:
+                    dx = 63
+                else :
+                    dx = 64
+                if y == 3:
+                    dy = 63
+                else :
+                    dy = 64
+                node_number = y*4 + x
+                node_tmp = Input_Node()
+                node_tmp.set_node_loc(node_x=1,node_y=0,node_number=node_number)
+                node_tmp.set_neu_par(spike_code='Count',reset=self.reset)
+                fm = [track_fm[i][64*x : 64*x+dx] for i in range(64*y, 64*y+dy)]
+                node_tmp.gen_input(filename="spike.txt",feature_map=fm)
+                
+        self.tik_send.gen_input(filename="spike.txt")
+
+class Transmitter(object):
+    def __init__(self):
+        self.socket_inst = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket_inst.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+
+    def connect_lwip(self, ip_address):
+        self.socket_inst.connect(ip_address)
+
+    def close(self):
+        self.socket_inst.close()
+        
+    def send_flit_bin(self, flit_bin_file, data_type):
+        '''
+        发送flit
+        '''
+        with open(flit_bin_file, 'rb') as file:
+            flit_bin = file.read()
+        length = len(flit_bin) >> 2
+        if length > 2**26:
+            print("===<2>=== %s is larger than 0.25GB" % flit_bin_file)
+            print("===<2>=== send flit length failed")
+            return 0
+        send_bytes = bytearray()
+        send_bytes += struct.pack('I', length)
+        send_bytes += struct.pack('I', data_type)
+        send_bytes += flit_bin
+        #self.socket_inst.sendall(struct.pack('I', length))
+        #ack = self.socket_inst.recv(1024)
+        #if (ack == b'done'):
+        #    print("===<2>=== send flit length succeed")
+        #self.socket_inst.sendall(flit_bin)
+        self.socket_inst.sendall(send_bytes)
+        return 1
+
+    def send_flit(self, flit_file, directions=0):
+        '''
+        发送flit
+        '''
+        with open(flit_file, 'r') as file:
+                flit_list = file.readlines()
+        length = len(flit_list)
+        if length > 2**26:
+            print("===<2>=== %s is larger than 0.25GB" % flit_file)
+            print("===<2>=== send flit length failed")
+            return 0
+        print("===<2>=== send flit length succeed")
+        #self.socket_inst.sendall(struct.pack('I', length))
+        #ack = self.socket_inst.recv(1024)
+        #if (ack == b'done'):
+        
+        j = 0
+        while(j < length):
+            send_bytes = bytearray()
+            send_bytes += struct.pack('Q', length)
+            # send_bytes += struct.pack('I', 0x8000) # TODO tik
+            for i in range(j,min(j + 16777216 * 4,length)):
+                send_bytes += struct.pack('Q', int(flit_list[i % length].strip(),16))
+            self.socket_inst.sendall(send_bytes)
+            j = j + 16777216 * 4
+            if (j <= length):
+                reply = self.socket_inst.recv(1024)
+                print("%s" % reply)
+        return 1    
+
+def run_pcss(tc="", filename="", recv = True, ip = "10.11.8.238", port=1): # 10.11.8.238
+    if tc != "" :
+        if os.path.exists(tc):
+            tc  = tc+"\\"
+        else:
+            tc = ""
+    trans = Transmitter()
+    ip_address = (ip, port) #TODO port
+    trans.connect_lwip(ip_address)
+    print("===<2>=== tcp connect succeed")
+    start_time = time.time_ns()
+    res=trans.send_flit(filename) # send
+    if res == 0:
+        return
+    end_time = time.time_ns()
+    print("===<2>=== send flit data   succeed")
+    print('===<2>=== tcp sent elapsed : %.3f ms' % ((end_time - start_time)/1000000))
+
+    f = open("flitout.txt", "wb") 
+    # fbin = open(tc+"recv_"+pre+"flitout.bin", "wb")
+    start_time = time.time_ns()
+    hl = b""
+    index = 0
+    tot = 0
+    while recv:
+        request = trans.socket_inst.recv(1024)
+        if len(request) <= 0:
+            break
+        # fbin.write(request)
+        for i in range(len(request)):
+            b = b"%02x" % request[i]
+            hl = b + hl
+            index = index + 1
+            if (index == 8):
+                f.write (hl + b"\n")
+                # print(hl)
+                hl = b""
+                index = 0
+                tot = tot + 1
+    end_time = time.time_ns()
+    f.close()
+    # fbin.close()
+    trans.socket_inst.close()
+    print('===<2>=== tcp recv elapsed : %.3f ms with %d flits' % ((end_time - start_time)/1000000,tot))
+
+if __name__ == "__main__":
+    # socket
+    id = 1
+    if len(sys.argv)>1:
+        id = int(sys.argv[1])
+    # init
+    net = PCSS()
+    net.init_pcss()
+    run_pcss(filename='config.txt', port=id)
+    # send fm
+    stime = time.time_ns()
+    net.gen_spk(track_fm=[])
+    run_pcss(filename='spike.txt', port=id)
+    etime = time.time_ns()
+    print("\n<--total time elapsed : %.3f ms-->\n" % ((etime - stime)/1000000.0))
