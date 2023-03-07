15d0dc5344c05b1e54c660a5204adc1fb146279c
liyt <491849663@qq.com>
2023-03-07 10:46:46 +0800

    read.txt

--- tb/tb_pcss.v
+++ tb/tb_pcss.v
@@ -117,7 +117,7 @@ end
 
 // open file
 initial begin
-    $readmemh("../pcss/read.txt",cfg_data);
+    $readmemh("../pcss/PCSS-main/read.txt",cfg_data);
     // $readmemh("D:/spike.txt",spk_data);
 end
 
